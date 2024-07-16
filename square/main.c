9672b541a5d0eae59bb563e51bd665ad3471ba4e
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2024-07-16 14:47:37 -0400

    Add files via upload

---
+++ square/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int elephanteatdinnermomz;
+
+int main()
+{
+    elephanteatdinnermomz i;
+    elephanteatdinnermomz x;
+    elephanteatdinnermomz j;
+    for (i = 0; i < 5; i++){
+    for(j=0;j<2;j++){printf(
+    "%0*d\n",20, 0  );x++;}}
+
+}
